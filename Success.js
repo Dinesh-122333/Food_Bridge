4751d6e353174a7c72a0755a7a659cbf368343fc
Akshaya Ponnuvel <akshayaponnuvel44@gmail.com>
2024-04-22 23:27:19 +0530

    completed upto success.js

--- Success.js
+++ Success.js
@@ -17,7 +17,7 @@ export default function Success({route, navigation}){
                     A suitable receiver will be in touch with you shortly.
                 </Text>
             </View>
-            <View>
+            <View style={styles.nextContainer}>
                 <TouchableOpacity style={styles.next} onPress={handleDone}>
                     <Text style={styles.textNext}>Done</Text>
                 </TouchableOpacity>
@@ -29,43 +29,54 @@ export default function Success({route, navigation}){
 const styles = StyleSheet.create({ 
     container: {
         flex: 1,
-        padding: 20,
         marginTop: 20,
         backgroundColor: 'white',
     },
     successImg:{
         alignSelf: 'center',
-        marginTop: 118,
+        marginTop:'40%',
+        height:200,
+        width:200,
     },
     successDescriptionContainer: {
         alignItems: 'center',
         justifyContent: 'center',
         marginTop: 50,
+        paddingLeft:20,
+        paddingRight:20,
     },
     successDescription: {
         fontSize: 18,
         fontWeight: '500',
         textAlign: 'center',
     },
+    nextContainer: {
+        marginTop: 'auto', // Push the container to the bottom
+        alignSelf: 'stretch', // Make sure the container stretches across the screen
+        alignItems: 'center', // Center the button horizontally
+        marginBottom: 25, // Add a margin at the bottom
+    },
     next: {
-        marginTop: 330,
-        paddingRight: 25,
-        paddingLeft: 30,
-        paddingVertical: 30,
-        borderColor: '#1B8BF5',
+        marginLeft: 20,
+        marginRight: 20,
+        paddingVertical: 28,
+        borderColor: "#3468C0",
         borderWidth: 2,
         borderRadius: 10,
-        backgroundColor: '#1B8BF5',
-    },
-    textNext: {
-        position: 'absolute',
-        fontSize: 16,
-        color: '#FFFFFF',
-        alignSelf: 'center',
-        marginTop: 16,
-    },
-    nextImg: {
-        marginLeft: 165,
-        marginTop: 3,
+        backgroundColor: "#3468C0",
+        flexDirection: 'row', // Align children vertically in the center
+        alignItems: 'center',
+        position: 'absolute', // Position absolutely
+        bottom: 0, // Position at the bottom with a margin
+        left: 0, // Align to the left
+        right: 0, // Align to the right
+    },    
+    textNext:{
+        position:"absolute",
+        fontSize:18,
+        color:"#FFFFFF",
+        alignSelf:"center",
+        marginTop:16,
+        marginLeft:'40%',
     },
 });
